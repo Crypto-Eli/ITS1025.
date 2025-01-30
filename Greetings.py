8b49ca630743f6d3aa0955426ce4b98244fcd0c9
Eliezer Timaiyo <156436326+Crypto-Eli@users.noreply.github.com>
2025-01-30 15:19:15 +0300

    Create Greetings.py

---
+++ Greetings.py
@@ -0,0 +1,10 @@
+
+# this is a personalized greeting program
+#the program will ask for your name and age
+#It then displays a personalized greeting 
+#prompt user for their name
+Username =input('what is your name ?')
+#prompt user for their age
+userage = input("what is your age ?")
+#display personalized greeting
+print('Hello',Username,'you are',userage,'years old')
